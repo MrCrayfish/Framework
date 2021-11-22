e634a5ce5086763580b2072ea1309b5a29d7abc3
MrCrayfish <mrcrayfish@hotmail.com>
2021-11-23 03:40:42 +1030

    📦 Set up basic mod structure

---
+++ src/main/java/com/mrcrayfish/framework/Framework.java
@@ -0,0 +1,19 @@
+package com.mrcrayfish.framework;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Author: MrCrayfish
+ */
+@Mod(Reference.MOD_ID)
+public class Framework
+{
+    public static final Logger LOGGER = LogManager.getLogger(Reference.MOD_ID);
+
+    public Framework()
+    {
+
+    }
+}
---
+++ src/main/java/com/mrcrayfish/framework/Reference.java
@@ -0,0 +1,9 @@
+package com.mrcrayfish.framework;
+
+/**
+ * Author: MrCrayfish
+ */
+public class Reference
+{
+    public static final String MOD_ID = "framework";
+}
