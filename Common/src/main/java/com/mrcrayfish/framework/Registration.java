bcca3b2b0d5ff3f6689d8a40ec9eb4c9e991d202
MrCrayfish <mrcrayfish@hotmail.com>
2023-03-25 16:02:10 +1030

    🐛 Fixed potential crash due to registration order

--- Common/src/main/java/com/mrcrayfish/framework/Registration.java
+++ Common/src/main/java/com/mrcrayfish/framework/Registration.java
@@ -2,13 +2,16 @@ package com.mrcrayfish.framework;
 
 import com.mrcrayfish.framework.api.registry.RegistryEntry;
 import com.mrcrayfish.framework.platform.Services;
+import net.minecraft.Util;
 import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,6 +22,24 @@ import java.util.stream.Collectors;
  */
 public final class Registration
 {
+    private static final Map<ResourceLocation, Integer> REGISTRATION_PRIORITY = Util.make(new HashMap<>(), map -> {
+        map.put(Registries.BLOCK.location(), 0);
+        map.put(Registries.ITEM.location(), 1);
+        map.put(Registries.FLUID.location(), 2);
+        map.put(Registries.MOB_EFFECT.location(), 3);
+        map.put(Registries.SOUND_EVENT.location(), 4);
+        map.put(Registries.POTION.location(), 5);
+        map.put(Registries.ENCHANTMENT.location(), 6);
+        map.put(Registries.ENTITY_TYPE.location(), 7);
+        map.put(Registries.BLOCK_ENTITY_TYPE.location(), 8);
+        map.put(Registries.PARTICLE_TYPE.location(), 9);
+        map.put(Registries.MENU.location(), 10);
+        map.put(Registries.RECIPE_TYPE.location(), 11);
+        map.put(Registries.RECIPE_SERIALIZER.location(), 12);
+        map.put(Registries.ATTRIBUTE.location(), 13);
+        map.put(Registries.COMMAND_ARGUMENT_TYPE.location(), 14);
+    });
+
     private static final Map<ResourceLocation, List<RegistryEntry<?>>> ENTRY_MAP = new HashMap<>();
 
     public static void init()
@@ -33,8 +54,11 @@ public final class Registration
         return ENTRY_MAP.getOrDefault(key.location(), Collections.emptyList());
     }
 
-    public static Collection<RegistryEntry<?>> getAllRegistryEntries()
+    public static List<RegistryEntry<?>> getAllRegistryEntries()
     {
-        return ENTRY_MAP.values().stream().flatMap(Collection::stream).collect(Collectors.toList());
+        return ENTRY_MAP.values().stream()
+                .flatMap(Collection::stream)
+                .sorted(Comparator.comparing(entry -> REGISTRATION_PRIORITY.getOrDefault(entry.getRegistry().key().location(), 1000)))
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 }
--- Fabric/src/test/java/test/registry/RegistryTest.java
+++ Fabric/src/test/java/test/registry/RegistryTest.java
@@ -3,7 +3,11 @@ package test.registry;
 import com.mrcrayfish.framework.api.registry.RegistryContainer;
 import com.mrcrayfish.framework.api.registry.RegistryEntry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 
 /**
  * Author: MrCrayfish
@@ -11,5 +15,6 @@ import net.minecraft.world.item.Item;
 @RegistryContainer
 public class RegistryTest
 {
-    public static final RegistryEntry<Item> THE_BEST_ITEM = RegistryEntry.item(new ResourceLocation("framework_test", "best_item"), () -> new Item(new Item.Properties()));
+    public static final RegistryEntry<Block> THE_BEST_BLOCK = RegistryEntry.block(new ResourceLocation("framework_test", "best_item"), () -> new Block(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)));
+    public static final RegistryEntry<Item> THE_BEST_ITEM = RegistryEntry.item(new ResourceLocation("framework_test", "best_item"), () -> new BlockItem(THE_BEST_BLOCK.get(), new Item.Properties()));
 }
