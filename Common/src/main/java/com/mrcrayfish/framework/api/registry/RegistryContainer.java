53b7886b675245e3fd2ba7436faf3d7633cdcd81
MrCrayfish <mrcrayfish@hotmail.com>
2023-06-10 11:08:26 +0930

    📦 Migrated to Guava to get registry containers

--- Common/src/main/java/com/mrcrayfish/framework/api/registry/RegistryContainer.java
+++ Common/src/main/java/com/mrcrayfish/framework/api/registry/RegistryContainer.java
@@ -1,8 +1,12 @@
 package com.mrcrayfish.framework.api.registry;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Author: MrCrayfish
  */
+@Retention(value = RetentionPolicy.RUNTIME)
 public @interface RegistryContainer
 {
 }
