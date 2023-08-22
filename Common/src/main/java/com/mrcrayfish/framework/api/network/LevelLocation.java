b43d0ca3474b6856599b040da63974706af96968
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2023-08-23 02:46:59 +0930

    ✨ Added more utility methods for sending packets

---
+++ Common/src/main/java/com/mrcrayfish/framework/api/network/LevelLocation.java
@@ -0,0 +1,47 @@
+package com.mrcrayfish.framework.api.network;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+
+/**
+ * Author: MrCrayfish
+ */
+public final class LevelLocation
+{
+    private final Level level;
+    private final Vec3 pos;
+    private final double range;
+
+    private LevelLocation(Level level, Vec3 pos, double range)
+    {
+        this.level = level;
+        this.pos = pos;
+        this.range = range;
+    }
+
+    public Level level()
+    {
+        return this.level;
+    }
+
+    public Vec3 pos()
+    {
+        return this.pos;
+    }
+
+    public double range()
+    {
+        return this.range;
+    }
+
+    public static LevelLocation create(Level level, BlockPos pos)
+    {
+        return new LevelLocation(level, pos.getCenter(), 16);
+    }
+
+    public static LevelLocation create(Level level, BlockPos pos, double range)
+    {
+        return new LevelLocation(level, pos.getCenter(), range);
+    }
+}
--- Forge/src/main/java/com/mrcrayfish/framework/platform/network/ForgeNetwork.java
+++ Forge/src/main/java/com/mrcrayfish/framework/platform/network/ForgeNetwork.java
@@ -1,10 +1,17 @@
 package com.mrcrayfish.framework.platform.network;
 
 import com.mrcrayfish.framework.api.network.FrameworkNetwork;
+import com.mrcrayfish.framework.api.network.LevelLocation;
 import com.mrcrayfish.framework.network.message.IMessage;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.SectionPos;
 import net.minecraft.network.Connection;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.chunk.LevelChunk;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.network.PacketDistributor;
 import net.minecraftforge.network.simple.SimpleChannel;
 
@@ -29,9 +36,53 @@ public class ForgeNetwork implements FrameworkNetwork
     }
 
     @Override
+    @Deprecated
     public void sendToTracking(Supplier<Entity> supplier, IMessage<?> message)
     {
+        this.sendToTrackingEntity(supplier, message);
+    }
+
+    @Override
+    public void sendToTrackingEntity(Supplier<Entity> supplier, IMessage<?> message)
+    {
         this.channel.send(PacketDistributor.TRACKING_ENTITY.with(supplier), message);
+    }
+
+    @Override
+    public void sendToTrackingBlockEntity(Supplier<BlockEntity> supplier, IMessage<?> message)
+    {
+        this.sendToTrackingChunk(() -> {
+            BlockEntity entity = supplier.get();
+            return entity.getLevel().getChunkAt(entity.getBlockPos());
+        }, message);
+    }
+
+    @Override
+    public void sendToTrackingLocation(Supplier<LevelLocation> supplier, IMessage<?> message)
+    {
+        this.sendToTrackingChunk(() -> {
+            LevelLocation location = supplier.get();
+            Vec3 pos = location.pos();
+            int chunkX = SectionPos.blockToSectionCoord(pos.x);
+            int chunkZ = SectionPos.blockToSectionCoord(pos.z);
+            return location.level().getChunk(chunkX, chunkZ);
+        }, message);
+    }
+
+    @Override
+    public void sendToTrackingChunk(Supplier<LevelChunk> supplier, IMessage<?> message)
+    {
+        this.channel.send(PacketDistributor.TRACKING_CHUNK.with(supplier), message);
+    }
+
+    @Override
+    public void sendToNearbyPlayers(Supplier<LevelLocation> supplier, IMessage<?> message)
+    {
+        this.channel.send(PacketDistributor.NEAR.with(() -> {
+            LevelLocation location = supplier.get();
+            Vec3 pos = location.pos();
+            return new PacketDistributor.TargetPoint(pos.x, pos.y, pos.z, location.range(), location.level().dimension());
+        }), message);
     }
 
     @Override
