53adf4b5147c23a39f541c7b810160b2f3b282e1
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2023-12-30 20:47:33 +1030

    ✨ Added API hook to open menus with custom data

--- common/src/main/java/com/mrcrayfish/framework/api/FrameworkAPI.java
+++ common/src/main/java/com/mrcrayfish/framework/api/FrameworkAPI.java
@@ -2,13 +2,20 @@ package com.mrcrayfish.framework.api;
 
 import com.mrcrayfish.framework.api.data.login.ILoginData;
 import com.mrcrayfish.framework.api.network.FrameworkNetworkBuilder;
+import com.mrcrayfish.framework.api.registry.RegistryEntry;
 import com.mrcrayfish.framework.api.sync.SyncedDataKey;
 import com.mrcrayfish.framework.entity.sync.SyncedEntityData;
 import com.mrcrayfish.framework.network.LoginDataManager;
 import com.mrcrayfish.framework.platform.Services;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.Entity;
+import org.apache.commons.lang3.function.TriFunction;
 
+import java.util.OptionalInt;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 /**
@@ -47,5 +54,20 @@ public class FrameworkAPI
     {
         // Internal code, do not call these directly since they may break in a future update.
         return Services.NETWORK.createNetworkBuilder(id, version);
+    }
+
+    /**
+     * Opens a menu with the ability to provide custom data to clients. The menu type of the menu must
+     * be an extended version as registered with {@link RegistryEntry#menuTypeWithData(ResourceLocation, TriFunction)}
+     * or an error will occur on the client.
+     *
+     * @param player   the player opening the menu
+     * @param provider the menu provider supplying the menu
+     * @param data     the custom data to be sent to the client
+     * @return an optional containing the window id or empty if failed to open
+     */
+    public static OptionalInt openMenuWithData(ServerPlayer player, MenuProvider provider, Consumer<FriendlyByteBuf> data)
+    {
+        return Services.NETWORK.openMenuWithData(player, provider, data);
     }
 }
