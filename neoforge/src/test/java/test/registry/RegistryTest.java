e9520b4481ace4fb22a0a3d9419dd250b6cf1c38
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2024-04-30 01:54:07 +0930

    📦 Added custom stat test to neoforge

--- neoforge/src/test/java/test/registry/RegistryTest.java
+++ neoforge/src/test/java/test/registry/RegistryTest.java
@@ -4,13 +4,19 @@ import com.mrcrayfish.framework.api.registry.RegistryContainer;
 import com.mrcrayfish.framework.api.registry.RegistryEntry;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.stats.StatFormatter;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.neoforged.fml.LogicalSide;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 
 /**
  * Author: MrCrayfish
@@ -20,6 +26,7 @@ import net.neoforged.fml.common.Mod;
 public class RegistryTest
 {
     public static final RegistryEntry<Block> MY_AWESOME_BLOCK = RegistryEntry.blockWithItem(new ResourceLocation("registry_test", "awesome_block"), () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS)));
+    public static final RegistryEntry<ResourceLocation> CUSTOM_AWESOME_STAT = RegistryEntry.customStat(new ResourceLocation("registry_test", "awesome_stat"), StatFormatter.DEFAULT);
 
     public static final RegistryEntry<CreativeModeTab> CUSTOM_TAB = RegistryEntry.creativeModeTab(new ResourceLocation("registry_test", "tab"), builder -> {
         builder.title(Component.literal("Creative tabs are pretty cool!"));
@@ -28,4 +35,21 @@ public class RegistryTest
             output.accept(new ItemStack(Items.STICK));
         });
     });
+
+    public RegistryTest()
+    {
+        NeoForge.EVENT_BUS.addListener(this::onLeftClickBlock);
+    }
+
+    private void onLeftClickBlock(PlayerInteractEvent.LeftClickBlock event)
+    {
+        if(event.getSide() != LogicalSide.SERVER)
+            return;
+
+        Player player = event.getEntity();
+        if(!(player instanceof ServerPlayer))
+            return;
+
+        player.awardStat(CUSTOM_AWESOME_STAT.get());
+    }
 }
