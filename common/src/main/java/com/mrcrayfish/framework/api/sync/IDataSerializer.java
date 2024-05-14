0fe9b6b307221685083b6461bbe258a55637f001
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2024-05-14 21:20:15 +0930

    📦 Fix some missing cases when supplying Updatable

--- common/src/main/java/com/mrcrayfish/framework/api/sync/IDataSerializer.java
+++ common/src/main/java/com/mrcrayfish/framework/api/sync/IDataSerializer.java
@@ -1,5 +1,6 @@
 package com.mrcrayfish.framework.api.sync;
 
+import com.mrcrayfish.framework.entity.sync.Updatable;
 import net.minecraft.nbt.Tag;
 import net.minecraft.network.FriendlyByteBuf;
 
@@ -10,9 +11,19 @@ public interface IDataSerializer<T>
 {
     void write(FriendlyByteBuf buf, T value);
 
+    @Deprecated(forRemoval = true, since = "0.7.6")
     T read(FriendlyByteBuf buf);
+
+    default T read(Updatable updatable, FriendlyByteBuf buf) {
+        return read(buf);
+    }
 
     Tag write(T value);
 
+    @Deprecated(forRemoval = true, since = "0.7.6")
     T read(Tag nbt);
+
+    default T read(Updatable updatable, Tag nbt) {
+        return read(nbt);
+    }
 }
