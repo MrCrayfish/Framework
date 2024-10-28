76fbd679da9ff211d2e8866ffa435ea152c2ee7f
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2024-10-28 16:18:44 +1030

    📦 Enforce loading from data to client only

--- common/src/main/java/com/mrcrayfish/framework/config/FrameworkConfigManager.java
+++ common/src/main/java/com/mrcrayfish/framework/config/FrameworkConfigManager.java
@@ -330,6 +330,7 @@ public class FrameworkConfigManager
 
         public boolean loadFromData(byte[] data)
         {
+            Preconditions.checkState(EnvironmentHelper.getEnvironment().isClient(), "Configs can only be loaded from data on the client");
             this.unload(false);
             try
             {
