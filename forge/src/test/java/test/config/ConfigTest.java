243f877135c5f60e6d672f885ab2aa205c7d5fdc
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2024-10-28 14:19:44 +1030

    📦 Add config test for Forge

---
+++ forge/src/test/java/test/config/ConfigTest.java
@@ -0,0 +1,35 @@
+package test.config;
+
+import com.mrcrayfish.framework.api.config.BoolProperty;
+import com.mrcrayfish.framework.api.config.ConfigProperty;
+import com.mrcrayfish.framework.api.config.ConfigType;
+import com.mrcrayfish.framework.api.config.EnumProperty;
+import com.mrcrayfish.framework.api.config.FrameworkConfig;
+import com.mrcrayfish.framework.api.config.IntProperty;
+import net.minecraft.core.Direction;
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Author: MrCrayfish
+ */
+@Mod("config_test")
+public class ConfigTest
+{
+    @FrameworkConfig(id = "config_test", name = "my_test_config")
+    public static final TestConfig TEST = new TestConfig();
+
+    @FrameworkConfig(id = "config_test", name = "my_test_sync_config", type = ConfigType.WORLD_SYNC)
+    public static final TestConfig TEST_SYNC = new TestConfig();
+
+    public static class TestConfig
+    {
+        @ConfigProperty(name = "canTouchGrass", comment = "Enables the ability to touch grass")
+        public final BoolProperty canTouchGrass = BoolProperty.create(true);
+
+        @ConfigProperty(name = "integerValue", comment = "A test integer with a valid range")
+        public final IntProperty true_LULW = IntProperty.create(0, 0, 10);
+
+        @ConfigProperty(name = "direction", comment = "A test enum property using direction")
+        public final EnumProperty<Direction> direction = EnumProperty.create(Direction.NORTH);
+    }
+}
