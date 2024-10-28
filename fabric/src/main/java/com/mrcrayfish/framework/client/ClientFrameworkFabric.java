d23eb723093160d28fec5e651d9cc5d1f48bc396
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2024-10-28 16:15:06 +1030

    📦 Switch to using own managed FileWatcher

--- fabric/src/main/java/com/mrcrayfish/framework/client/ClientFrameworkFabric.java
+++ fabric/src/main/java/com/mrcrayfish/framework/client/ClientFrameworkFabric.java
@@ -3,6 +3,7 @@ package com.mrcrayfish.framework.client;
 import com.mrcrayfish.framework.Constants;
 import com.mrcrayfish.framework.FrameworkData;
 import com.mrcrayfish.framework.api.event.InputEvents;
+import com.mrcrayfish.framework.config.ConfigWatcher;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
@@ -45,6 +46,9 @@ public class ClientFrameworkFabric implements ClientModInitializer
 
         ClientLifecycleEvents.CLIENT_STARTED.register(client -> {
             FrameworkData.setLoaded();
+        });
+        ClientLifecycleEvents.CLIENT_STOPPING.register(client -> {
+            ConfigWatcher.get().stop();
         });
     }
 }
