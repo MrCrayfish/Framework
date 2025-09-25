81c049187ec0904fbc79a9e1ad5cb68fd55c9406
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-09-25 23:43:41 +0930

    📦 Break down FrameworkRegistry into a wrapped base

--- common/src/main/java/com/mrcrayfish/framework/api/registry/FrameworkRegistry.java
+++ common/src/main/java/com/mrcrayfish/framework/api/registry/FrameworkRegistry.java
@@ -1,32 +1,18 @@
 package com.mrcrayfish.framework.api.registry;
 
-import com.mrcrayfish.framework.registry.RegistryProxy;
-import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.Iterator;
-
-public final class FrameworkRegistry<T> implements Iterable<T>
+public final class FrameworkRegistry<T> extends WrappedRegistry<T>
 {
-    private final ResourceKey<Registry<T>> key;
     private final boolean save;
     private final boolean sync;
-    private @Nullable RegistryProxy<T> proxy;
 
     private FrameworkRegistry(ResourceLocation id, boolean save, boolean sync)
     {
-        this.key = ResourceKey.createRegistryKey(id);
+        super(ResourceKey.createRegistryKey(id));
         this.save = save;
         this.sync = sync;
-    }
-
-    public ResourceKey<Registry<T>> getKey()
-    {
-        return this.key;
     }
 
     public boolean shouldSave()
@@ -38,36 +24,6 @@ public final class FrameworkRegistry<T> implements Iterable<T>
     {
         return this.sync;
     }
-
-    @NotNull
-    @Override
-    public Iterator<T> iterator()
-    {
-        return this.getProxy().iterable().iterator();
-    }
-
-    @ApiStatus.Internal
-    RegistryProxy<T> getProxy()
-    {
-        if(this.proxy == null)
-            throw new IllegalStateException("Registry not created yet");
-        return this.proxy;
-    }
-
-    @ApiStatus.Internal
-    @SuppressWarnings("unchecked")
-    public void setProxy(RegistryProxy<?> registry)
-    {
-        this.proxy = (RegistryProxy<T>) registry;
-    }
-
-    /*@ApiStatus.Internal
-    public static <T> FrameworkRegistry<T> wrapVanilla(Registry<T> registry)
-    {
-        FrameworkRegistry<T> r = new FrameworkRegistry<>(registry.key().location(), false, false);
-        r.setProxy(VanillaRegistryProxy.wrap(registry));
-        return r;
-    }*/
 
     public static <T> Builder<T> builder(ResourceLocation id)
     {
