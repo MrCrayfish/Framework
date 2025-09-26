b503af65c4833894a1033a197fc57025e672346c
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-09-26 17:22:39 +0930

    📦 Add docs

--- common/src/main/java/com/mrcrayfish/framework/api/registry/RegistryContainer.java
+++ common/src/main/java/com/mrcrayfish/framework/api/registry/RegistryContainer.java
@@ -12,6 +12,9 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RegistryContainer
 {
+    /**
+     * @return True if this registry container should only be detected if in a client environment
+     */
     @SuppressWarnings("unused")
     boolean clientOnly() default false;
 }
