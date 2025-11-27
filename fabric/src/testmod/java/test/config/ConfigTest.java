29b7a252ff388a879fe28033a83d06adf997ea68
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-11-28 00:00:34 +1030

    📦 Add test for resource location validator

--- fabric/src/testmod/java/test/config/ConfigTest.java
+++ fabric/src/testmod/java/test/config/ConfigTest.java
@@ -2,6 +2,7 @@ package test.config;
 
 import com.mrcrayfish.framework.api.config.*;
 import com.mrcrayfish.framework.api.config.validate.NumberRange;
+import com.mrcrayfish.framework.api.config.validate.ResourceLocationValidator;
 import net.minecraft.core.Direction;
 
 import java.util.List;
@@ -38,6 +39,12 @@ public class ConfigTest
 
             @ConfigProperty(name = "myStringList", comment = "A list of strings")
             public final ListProperty<String> stringList = ListProperty.create(ListProperty.STRING, () -> List.of("Hello", "World"));
+
+            @ConfigProperty(name = "myItemsList", comment = "A list of item identifiers")
+            public final ListProperty<String> itemsList = ListProperty.create(ListProperty.STRING, ResourceLocationValidator.any(), () -> List.of("minecraft:stick", "minecraft:stone"));
+
+            @ConfigProperty(name = "paymentItem", comment = "The item to use for payments. Cannot be empty")
+            public final StringProperty paymentItem = StringProperty.create("minecraft:emerald", ResourceLocationValidator.restrictNamespace("minecraft"));
         }
     }
 }
