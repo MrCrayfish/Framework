16024ee32177d3d0612ab205a0f677e21123e00c
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-11-29 19:32:01 +1030

    📦 Add tooltip option to rebuild on hover

--- common/src/main/java/com/mrcrayfish/framework/api/client/screen/TooltipOptions.java
+++ common/src/main/java/com/mrcrayfish/framework/api/client/screen/TooltipOptions.java
@@ -5,5 +5,5 @@ public final class TooltipOptions
 {
     public static final int DISABLE_TOOLTIP_WHEN_WIDGET_INACTIVE = 1 << 0;
     public static final int REBUILD_TOOLTIP_ON_SHIFT = 1 << 1;
-    //public static final int REBUILD_TOOLTIP_ON_INITIAL_HOVER = 1 << 2;
+    public static final int REBUILD_TOOLTIP_ON_WIDGET_HOVER = 1 << 2;
 }
