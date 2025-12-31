acc0bb982f57340593fdef4d8e0d4504cbc6eec0
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-12-31 14:48:45 +1030

    📦 Clean up Buttons helper

--- common/src/main/java/com/mrcrayfish/framework/api/client/screen/Buttons.java
+++ common/src/main/java/com/mrcrayfish/framework/api/client/screen/Buttons.java
@@ -1,57 +1,92 @@
 package com.mrcrayfish.framework.api.client.screen;
 
-import com.google.common.annotations.Beta;
 import com.mrcrayfish.framework.api.client.screen.widget.FrameworkButton;
-import com.mrcrayfish.framework.api.util.LabelAndDescription;
 import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public final class Buttons
 {
     private Buttons() {}
 
-    public static FrameworkButton.Builder createOnOffOption(Component label, Supplier<Boolean> getter, Consumer<Boolean> setter)
+    public static FrameworkButton.Builder createOnOff(Supplier<Boolean> getter, Consumer<Boolean> setter)
     {
-        return createOnOffOption(label, getter, setter, value -> {});
+        return createOnOff(getter, setter, newValue -> {});
     }
 
-    public static FrameworkButton.Builder createOnOffOption(Component label, Supplier<Boolean> getter, Consumer<Boolean> setter, Consumer<Boolean> callback)
-    {
-        return FrameworkButton.builder().setAction(btn -> {
-            boolean newValue = !getter.get();
-            setter.accept(newValue);
-            callback.accept(newValue);
-        }).setLabel(() -> CommonComponents.optionStatus(label, getter.get()));
-    }
-
-    public static FrameworkButton.Builder toggle(Supplier<Boolean> getter, Consumer<Boolean> setter)
-    {
-        return toggle(getter, setter, newValue -> {});
-    }
-
-    public static FrameworkButton.Builder toggle(Supplier<Boolean> getter, Consumer<Boolean> setter, Consumer<Boolean> onChanged)
+    public static FrameworkButton.Builder createOnOff(Supplier<Boolean> getter, Consumer<Boolean> setter, Consumer<Boolean> onChanged)
     {
         return FrameworkButton.builder().setAction(btn -> {
             boolean newValue = !getter.get();
             setter.accept(newValue);
             onChanged.accept(newValue);
-        }).setContentRenderer(new FrameworkButton.ToggleContentRenderer(getter));
+        }).setLabel(() -> CommonComponents.optionStatus(getter.get()));
     }
 
-    public static <T extends Enum<T> & LabelAndDescription> FrameworkButton.Builder values(Supplier<T> getter, Consumer<T> setter)
+    public static FrameworkButton.Builder createOnOff(Component label, Supplier<Boolean> getter, Consumer<Boolean> setter)
     {
-        return values(getter, setter, t -> {});
+        return createOnOff(label, getter, setter, value -> {});
     }
 
-    public static <T extends Enum<T> & LabelAndDescription> FrameworkButton.Builder values(Supplier<T> getter, Consumer<T> setter, Consumer<T> onChanged)
+    public static FrameworkButton.Builder createOnOff(Component label, Supplier<Boolean> getter, Consumer<Boolean> setter, Consumer<Boolean> onChanged)
+    {
+        return FrameworkButton.builder().setAction(btn -> {
+            boolean newValue = !getter.get();
+            setter.accept(newValue);
+            onChanged.accept(newValue);
+        }).setLabel(() -> CommonComponents.optionStatus(label, getter.get()));
+    }
+
+    public static FrameworkButton.Builder createToggle(Supplier<Boolean> getter, Consumer<Boolean> setter)
+    {
+        return createToggle(CommonComponents.EMPTY, getter, setter, newValue -> {});
+    }
+
+    public static FrameworkButton.Builder createToggle(Supplier<Boolean> getter, Consumer<Boolean> setter, Consumer<Boolean> onChanged)
+    {
+        return createToggle(CommonComponents.EMPTY, getter, setter, onChanged);
+    }
+
+    public static FrameworkButton.Builder createToggle(Component label, Supplier<Boolean> getter, Consumer<Boolean> setter)
+    {
+        return createToggle(label, getter, setter, newValue -> {});
+    }
+
+    public static FrameworkButton.Builder createToggle(Component label, Supplier<Boolean> getter, Consumer<Boolean> setter, Consumer<Boolean> onChanged)
     {
         return FrameworkButton.builder()
-            .setLabel(() -> getter.get().label())
-            .setTooltip(btn -> Tooltip.create(getter.get().description()))
+            .setContentRenderer(new FrameworkButton.ToggleContentRenderer(getter))
+            .setLabel(label)
+            .setAction(btn -> {
+                boolean newValue = !getter.get();
+                setter.accept(newValue);
+                onChanged.accept(newValue);
+            });
+    }
+
+    public static <T extends Enum<T>> FrameworkButton.Builder createValues(Function<T, Component> label, Supplier<T> getter, Consumer<T> setter)
+    {
+        return createValues(label, t -> null, getter, setter, newValue -> {});
+    }
+
+    public static <T extends Enum<T>> FrameworkButton.Builder createValues(Function<T, Component> label, Supplier<T> getter, Consumer<T> setter, Consumer<T> onChanged)
+    {
+        return createValues(label, t -> null, getter, setter, onChanged);
+    }
+
+    public static <T extends Enum<T>> FrameworkButton.Builder createValues(Function<T, Component> label, Function<T, @Nullable Component> tooltip, Supplier<T> getter, Consumer<T> setter, Consumer<T> onChanged)
+    {
+        return FrameworkButton.builder()
+            .setLabel(() -> label.apply(getter.get()))
+            .setTooltip(btn -> {
+                Component text = tooltip.apply(getter.get());
+                return text != null ? Tooltip.create(text) : null;
+            })
             .setAction(btn -> {
                 T currentValue = getter.get();
                 T[] values = currentValue.getDeclaringClass().getEnumConstants();
