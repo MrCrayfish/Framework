91b8413536540a2ca2a3a9aba757baad36c4b3ea
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2026-01-02 16:01:46 +1030

    📦 Add javadocs to Action

--- common/src/main/java/com/mrcrayfish/framework/api/client/screen/widget/input/Action.java
+++ common/src/main/java/com/mrcrayfish/framework/api/client/screen/widget/input/Action.java
@@ -23,22 +23,42 @@ public final class Action<T>
         this.sound = sound;
     }
 
+    /**
+     * @return The handler to execute when this action is triggered
+     */
     public Consumer<T> handler()
     {
         return this.handler;
     }
 
+    /**
+     * @return The sound to play when this action is triggered, or null if no sound should be played
+     */
     @Nullable
     public Sound sound()
     {
         return this.sound;
     }
 
+    /**
+     * Creates a new {@link Action} with the specified {@link Consumer} handler. This action will
+     * play the default UI click sound, {@link SoundEvents#UI_BUTTON_CLICK}.
+     *
+     * @param action a {@link Consumer} to execute when this action is triggered
+     * @return the created {@link Action} instance
+     */
     public static <T> Action<T> create(Consumer<T> action)
     {
         return new Action<>(action, DEFAULT_SOUND);
     }
 
+    /**
+     * Creates a new {@link Action} with the specified {@link Consumer} handler and custom sound.
+     *
+     * @param action a {@link Consumer} to execute when this action is triggered
+     * @param sound  a {@link Sound} to play when this action is triggered, or null if no sound should be played
+     * @return the created {@link Action} instance
+     */
     public static <T> Action<T> create(Consumer<T> action, @Nullable Sound sound)
     {
         return new Action<>(action, sound);
