104a4b26c2a3f9796eafd78ec4a2921312c86280
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2026-01-02 15:27:27 +1030

    📦 Action sounds can now be null

--- common/src/main/java/com/mrcrayfish/framework/api/client/screen/widget/input/Action.java
+++ common/src/main/java/com/mrcrayfish/framework/api/client/screen/widget/input/Action.java
@@ -5,6 +5,7 @@ import com.mrcrayfish.framework.api.client.screen.widget.element.Sound;
 import net.minecraft.core.Holder;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Consumer;
 
@@ -14,9 +15,9 @@ public final class Action<T>
     private static final Sound DEFAULT_SOUND = Sound.create(SoundEvents.UI_BUTTON_CLICK);
 
     private final Consumer<T> handler;
-    private final Sound sound;
+    private final @Nullable Sound sound;
 
-    private Action(Consumer<T> handler, Sound sound)
+    private Action(Consumer<T> handler, @Nullable Sound sound)
     {
         this.handler = handler;
         this.sound = sound;
@@ -27,6 +28,7 @@ public final class Action<T>
         return this.handler;
     }
 
+    @Nullable
     public Sound sound()
     {
         return this.sound;
@@ -37,7 +39,7 @@ public final class Action<T>
         return new Action<>(action, DEFAULT_SOUND);
     }
 
-    public static <T> Action<T> create(Consumer<T> action, Sound sound)
+    public static <T> Action<T> create(Consumer<T> action, @Nullable Sound sound)
     {
         return new Action<>(action, sound);
     }
