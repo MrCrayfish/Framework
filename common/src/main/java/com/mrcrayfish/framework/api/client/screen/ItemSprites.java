199aa92fbad57fbc798afc3aa15708e4b7f82c72
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2026-01-02 17:19:45 +1030

    📦 Add javadocs to ItemSprites

--- common/src/main/java/com/mrcrayfish/framework/api/client/screen/ItemSprites.java
+++ common/src/main/java/com/mrcrayfish/framework/api/client/screen/ItemSprites.java
@@ -41,26 +41,50 @@ public final class ItemSprites
         return key;
     }
 
+    /**
+     * Retrieves the sprite texture resource location based on the provided state parameters.
+     *
+     * @param enabled  indicates the enabled state
+     * @param hovered  indicates the hovered state
+     * @param selected indicates the selected state
+     * @return a {@link ResourceLocation} pointing to a texture based on the given states, or null
+     */
+    @Nullable
     public ResourceLocation getSprite(boolean enabled, boolean hovered, boolean selected)
     {
         return this.map.get(this.calculateKey(enabled, hovered, selected));
     }
 
+    /**
+     * Creates a new {@link ItemSprites} where all sprite states point to the same texture location.
+     * Alternatively you can use the {@link Builder} to configure the sprites individually.
+     *
+     * @param all the resource location to be used for all sprite states; can be null
+     * @return a new instance of {@link ItemSprites} with the same resource location for all states
+     */
     public static ItemSprites of(@Nullable ResourceLocation all)
     {
         return new ItemSprites(all, all, all, all, all, all, all, all);
     }
 
+    /**
+     * Creates a new {@link ItemSprites} that matches the states of a vanilla button (enabled, disabled
+     * and selected (focused)). Alternatively you can use the {@link Builder} to configure the sprites
+     * individually.
+     *
+     * @param enabled         the resource location to a texture for the enabled state, or null for no texture
+     * @param disabled        the resource location to a texture for the disabled state, or null for no texture
+     * @param enabledSelected the resource location to a texture for the enabled and selected state, or null for no texture
+     * @return a new {@link ItemSprites}  instance initialized with the provided resource locations
+     */
     public static ItemSprites of(@Nullable ResourceLocation enabled, @Nullable ResourceLocation disabled, @Nullable ResourceLocation enabledSelected)
     {
         return new ItemSprites(enabled, disabled, enabled, disabled, enabledSelected, disabled, enabledSelected, disabled);
     }
 
-    public static ItemSprites of(@Nullable ResourceLocation enabled, @Nullable ResourceLocation disabled, @Nullable ResourceLocation enabledHovered, @Nullable ResourceLocation disabledHovered, @Nullable ResourceLocation enabledSelected, @Nullable ResourceLocation disabledSelected, @Nullable ResourceLocation enabledHoveredSelected, @Nullable ResourceLocation disabledHoveredSelected)
-    {
-        return new ItemSprites(enabled, disabled, enabledHovered, disabledHovered, enabledSelected, disabledSelected, enabledHoveredSelected, disabledHoveredSelected);
-    }
-
+    /**
+     * @return A new {@link Builder} to configure and build an {@link ItemSprites} instance
+     */
     public static Builder builder()
     {
         return new Builder();
@@ -77,56 +101,109 @@ public final class ItemSprites
         private @Nullable ResourceLocation enabledHoveredSelected;
         private @Nullable ResourceLocation disabledHoveredSelected;
 
+        /**
+         * Builds an {@link ItemSprites} instance with the configured sprites
+         *
+         * @return a new {@link ItemSprites} instance
+         */
         public ItemSprites build()
         {
             return new ItemSprites(this.enabled, this.disabled, this.enabledHovered, this.disabledHovered, this.enabledSelected, this.disabledSelected, this.enabledHoveredSelected, this.disabledHoveredSelected);
         }
 
-        public Builder setEnabled(@Nullable ResourceLocation enabled)
+        /**
+         * Sets the sprite for the enabled state
+         *
+         * @param texture the resource location to a sprite texture, or null
+         * @return this {@link Builder} for method chaining
+         */
+        public Builder setEnabled(@Nullable ResourceLocation texture)
         {
-            this.enabled = enabled;
+            this.enabled = texture;
             return this;
         }
 
-        public Builder setDisabled(@Nullable ResourceLocation disabled)
+        /**
+         * Sets the sprite for the disabled state.
+         *
+         * @param texture the resource location to a sprite texture, or null
+         * @return this {@link Builder} for method chaining
+         */
+        public Builder setDisabled(@Nullable ResourceLocation texture)
         {
-            this.disabled = disabled;
+            this.disabled = texture;
             return this;
         }
 
-        public Builder setEnabledHovered(@Nullable ResourceLocation enabledHovered)
+        /**
+         * Sets the sprite for the enabled and hovered state
+         *
+         * @param texture the resource location to a sprite texture, or null
+         * @return this {@link Builder} for method chaining
+         */
+        public Builder setEnabledHovered(@Nullable ResourceLocation texture)
         {
-            this.enabledHovered = enabledHovered;
+            this.enabledHovered = texture;
             return this;
         }
 
-        public Builder setDisabledHovered(@Nullable ResourceLocation disabledHovered)
+        /**
+         * Sets the sprite for the disabled and hovered state
+         *
+         * @param texture the resource location to a sprite texture, or null
+         * @return this {@link Builder} for method chaining
+         */
+        public Builder setDisabledHovered(@Nullable ResourceLocation texture)
         {
-            this.disabledHovered = disabledHovered;
+            this.disabledHovered = texture;
             return this;
         }
 
-        public Builder setEnabledSelected(@Nullable ResourceLocation enabledSelected)
+        /**
+         * Sets the sprite for the enabled and selected state
+         *
+         * @param texture the resource location to a sprite texture, or null
+         * @return this {@link Builder} for method chaining
+         */
+        public Builder setEnabledSelected(@Nullable ResourceLocation texture)
         {
-            this.enabledSelected = enabledSelected;
+            this.enabledSelected = texture;
             return this;
         }
 
-        public Builder setDisabledSelected(@Nullable ResourceLocation disabledSelected)
+        /**
+         * Sets the sprite for the disabled and selected state
+         *
+         * @param texture the resource location to a sprite texture, or null
+         * @return this {@link Builder} for method chaining
+         */
+        public Builder setDisabledSelected(@Nullable ResourceLocation texture)
         {
-            this.disabledSelected = disabledSelected;
+            this.disabledSelected = texture;
             return this;
         }
 
-        public Builder setEnabledHoveredSelected(@Nullable ResourceLocation enabledHoveredSelected)
+        /**
+         * Sets the sprite for the enabled, hovered, and selected state
+         *
+         * @param texture the resource location to a sprite texture, or null
+         * @return this {@link Builder} for method chaining
+         */
+        public Builder setEnabledHoveredSelected(@Nullable ResourceLocation texture)
         {
-            this.enabledHoveredSelected = enabledHoveredSelected;
+            this.enabledHoveredSelected = texture;
             return this;
         }
 
-        public Builder setDisabledHoveredSelected(@Nullable ResourceLocation disabledHoveredSelected)
+        /**
+         * Sets the sprite for the disabled, hovered, and selected state
+         *
+         * @param texture the resource location to a sprite texture, or null
+         * @return this {@link Builder} for method chaining
+         */
+        public Builder setDisabledHoveredSelected(@Nullable ResourceLocation texture)
         {
-            this.disabledHoveredSelected = disabledHoveredSelected;
+            this.disabledHoveredSelected = texture;
             return this;
         }
     }
