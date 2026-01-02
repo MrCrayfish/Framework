b8768cce0b366417aee4b62a0fb9428449967cf6
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2026-01-02 18:40:20 +1030

    💥 Remove `@Beta` from Icon and TooltipOptions

--- common/src/main/java/com/mrcrayfish/framework/api/client/screen/TooltipOptions.java
+++ common/src/main/java/com/mrcrayfish/framework/api/client/screen/TooltipOptions.java
@@ -1,11 +1,8 @@
 package com.mrcrayfish.framework.api.client.screen;
 
-import com.google.common.annotations.Beta;
-
 /**
  * Tooltip options for widgets. These options are intended to be applied together using bitwise ops.
  */
-@Beta
 @SuppressWarnings("PointlessBitwiseExpression")
 public final class TooltipOptions
 {
