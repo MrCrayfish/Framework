0965281adf0d2a3c7a97dd805f25d740748a962c
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2026-05-20 02:50:51 +0930

    🐛 Fix text not trimmed on select

--- common/src/main/java/com/mrcrayfish/framework/api/client/screen/widget/FrameworkSelect.java
+++ common/src/main/java/com/mrcrayfish/framework/api/client/screen/widget/FrameworkSelect.java
@@ -303,13 +303,13 @@ public final class FrameworkSelect<T> extends AbstractWidget
                 int iconX = this.getX() + this.getWidth() - icon.width() - margin;
                 int iconY = this.getY() + margin;
                 icon.draw(graphics, iconX, iconY, partialTick);
-                textEnd = iconX;
+                textEnd = this.getWidth() - icon.width() - margin;
             }
         }
         if(this.selected != null)
         {
             int margin = Math.max((this.getHeight() - 8) / 2, 0);
-            int maxWidth = (textEnd == -1 ? this.getWidth() - margin : textEnd - 5) - margin;
+            int maxWidth = (textEnd == -1 ? this.getWidth() - margin: textEnd - 4) - margin;
             Font font = Minecraft.getInstance().font;
             String label = this.selected.label.getString();
             if(font.width(label) > maxWidth)
