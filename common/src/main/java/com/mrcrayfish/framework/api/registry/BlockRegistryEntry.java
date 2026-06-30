d6f6120f3f6593dfc372f01bea3031e13b179adf
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2026-06-30 17:11:39 +0930

    📦 Add methods to BlockRegistryEntry to get block and item resource keys

--- common/src/main/java/com/mrcrayfish/framework/api/registry/BlockRegistryEntry.java
+++ common/src/main/java/com/mrcrayfish/framework/api/registry/BlockRegistryEntry.java
@@ -1,9 +1,11 @@
 package com.mrcrayfish.framework.api.registry;
 
 import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import org.jetbrains.annotations.NotNull;
 
@@ -17,12 +19,16 @@ import java.util.function.Supplier;
 public final class BlockRegistryEntry<T extends Block, E extends BlockItem> extends RegistryEntry<T>
 {
     private final Function<T, E> itemSupplier;
+    private final ResourceKey<@NotNull Block> blockKey;
+    private final ResourceKey<@NotNull Item> itemKey;
     private E itemInstance = null;
 
     BlockRegistryEntry(Registry<?> registry, ResourceKey<@NotNull Block> key, Supplier<T> blockSupplier, Function<T, E> itemSupplier)
     {
         super(registry, key, blockSupplier);
         this.itemSupplier = itemSupplier;
+        this.blockKey = key;
+        this.itemKey = ResourceKey.create(Registries.ITEM, key.identifier());
     }
 
     @Override
@@ -43,5 +49,15 @@ public final class BlockRegistryEntry<T extends Block, E extends BlockItem> exte
     public Optional<E> item()
     {
         return Optional.ofNullable(this.itemInstance);
+    }
+
+    public ResourceKey<@NotNull Block> getBlockKey()
+    {
+        return this.blockKey;
+    }
+
+    public ResourceKey<@NotNull Item> getItemKey()
+    {
+        return this.itemKey;
     }
 }
